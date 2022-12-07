c054744aaf4581d147ffd445bc90a56484a7478c
Connorbelez <connor.belez@gmail.com>
2022-12-07 11:07:35 -0500

    Day7p2

--- Day7/sols.py
+++ Day7/sols.py
@@ -19,7 +19,7 @@ class Directory:
         return self.children[dir]
     
     def __str__(self):
-        return "Name: " + self.name + " Size: " + str(self.size) + " Parent: " + str(self.parent) + " Children: " + str(self.children)
+        return "Name: " + self.name + " Size: " + str(self.size)
         
 
 def sol1(file):
@@ -57,8 +57,11 @@ def sol1(file):
                         Dir.add_child(newDir)
                     else:
                         Dir.update_size(int(l[0]))
-
-    return bfs(root)
+    
+    sizeReq = 30000000
+    DiscAvail = 70000000 - root.size
+    val = sizeReq - DiscAvail
+    return bfs(root), bfs2(root,val)
         
         
 def bfs(root):
@@ -73,22 +76,29 @@ def bfs(root):
     return counter
 
 #create a bfs function that returns the number of directories with size less than 100000
+def bfs2(root,sizeReq):
+    q = [root]
+    counter = 0
+    canidate = [root]
+    min1 = root.size
+    while q:
+        node = q.pop(0)
+        if node.size >= sizeReq and node.size < min1:
+            canidate.append(node)
+            min1 = node.size
+        for childName,child in node.children.items():
+            q.append(child)
+    for i in canidate:
+        print(i)
+    return min1
 
-#create a bredth first search function
-
-        
-def sol2(file):
-    with open(file, "r") as f:
-        lines = f.readlines()
-        for line in f:
-            pass
-            
             
 
 def main():
     file = "/Users/connorbeleznay/Projects/Personal/AdventOfCode/2022/Day7/data.txt"
-    print(sol1(file))
-    print(sol2(file))
+    num, num2 = sol1(file)
+    print(num)
+    print(num2)
     
 if __name__ == "__main__":
     main()
